7a601820b7d4053f46d274e551a34c3b51bbd8b6
Dimitrius Borges <dimitriusborges@gmail.com>
2023-10-10 09:01:00 -0300

    re-styling banner component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -5,7 +5,7 @@ import circuloCololorido from 'assets/circulo_colorido.png'
 export default function Banner() {
     return (
         <div className={styles.banner}>
-            <div className={styles.apresentacao}>
+            <div>
                 <h1 className={styles.titulo}>
                     Hi!
                 </h1>
@@ -13,17 +13,18 @@ export default function Banner() {
                 <p className={styles.paragrafo}>
                     Welcome to my Personal Blog! My name is Dimitrius Borges and I'm a React student. Here I will share some of my recently acquired knowledges
                 </p>
-                <div className={styles.imagens}>
-                    <img className={styles.circuloColorido}
-                         src={circuloCololorido}
-                         aria-hidden={true}
-                         alt="background element"
-                    />
-                </div>
-                    <img className={styles.minhaFoto}
-                         src="https://github.com/dimitriusborges.png"
-                         alt="Dimitrius selfie"
-                    />
+            </div>
+            <div className={styles.imagens}>
+                <img className={styles.circuloColorido}
+                     src={circuloCololorido}
+                     aria-hidden={true}
+                     alt="background element"
+                />
+
+                <img className={styles.minhaFoto}
+                     src="https://github.com/dimitriusborges.png"
+                     alt="Dimitrius selfie"
+                />
             </div>
         </div>
     )
